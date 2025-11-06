36b5ed18df041f075775c9ef504376ca98eed3ad
selvarahul <iamselvarahul@gmail.com>
2025-11-06 23:50:26 +0530

    Restore NotFound page, add router basename and update vite config

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/home/Home";
 import { useRef, useState } from "react";
 import GenreFetch from "./components/genre/GenreFetch";
 import TopRated from "./components/toprated/TopRated";
+import NotFound from "./NotFound";
 import UpComing from "./components/upcoming/UpComing";
 import NowPlaying from "./components/nowplaying/NowPlaying";
 import SearchMovies from "./components/searchmovies/SearchMovies";
@@ -44,6 +45,7 @@ function App() {
           <Route path="thriller" element={<GenreFetch genreId={53} current={current} setCurrent={setCurrent}/>}/>
           <Route path="war" element={<GenreFetch genreId={10752} current={current} setCurrent={setCurrent}/>}/>
           <Route path="western" element={<GenreFetch genreId={37} current={current} setCurrent={setCurrent}/>}/>
+          <Route path="*" element={<NotFound/>}/>
         </Route>
         
         {/* Standalone route without Header and Footer */}
